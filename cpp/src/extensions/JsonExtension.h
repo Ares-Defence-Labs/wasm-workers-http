f8e5a5521884a3e2f2d305a4e2cfa3be5faf44b5
TheArchitect123 <dan.developer789@gmail.com>
2026-03-13 23:31:04 +1100

    Update JsonExtension.h

--- cpp/src/extensions/JsonExtension.h
+++ cpp/src/extensions/JsonExtension.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <map>
-#include <memory>
 #include <string>
 
 #include "models/HttpResponse.hpp"
@@ -12,25 +10,13 @@ using json = nlohmann::json;
 namespace AresWasmWorker {
     struct JsonExtensions {
         template<ResponseChecker BodyType>
-        static HttpResponse<BodyType> getResponseFromJson(const std::string& responseJson) {
-            auto j = nlohmann::json::parse(responseJson);
-            auto status = j.at("status").get<uint32_t>();
-
-            auto headers = std::make_shared<std::map<std::string, std::string>>(
-                j.at("headers").get<std::map<std::string, std::string>>()
-            );
-
-            auto body = j.at("body").get<BodyType>();
-
-            return HttpResponse<BodyType>(status, headers, body);
+        static BodyType getResponseFromJson(const std::string& responseJson) {
+            return nlohmann::json::parse(responseJson).get<BodyType>();
         }
 
         template<ResponseChecker BodyType>
-        static HttpResponse<BodyType> getResponseBodyFromJson(const std::string& bodyJson) {
-            auto responseBody = nlohmann::json::parse(bodyJson).get<BodyType>();
-
-            auto headers = std::make_shared<std::map<std::string, std::string>>();
-            return HttpResponse<BodyType>(0, headers, responseBody);
+        static BodyType getResponseBodyFromJson(const std::string& bodyJson) {
+            return nlohmann::json::parse(bodyJson).get<BodyType>();
         }
     };
 }
