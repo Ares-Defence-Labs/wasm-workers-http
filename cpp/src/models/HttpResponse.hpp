0e615defae841fb932ea82e2123f4e52058fb0df
TheArchitect123 <dan.developer789@gmail.com>
2026-03-13 16:43:09 +1100

    Update HttpResponse.hpp

--- cpp/src/models/HttpResponse.hpp
+++ cpp/src/models/HttpResponse.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <map>
-#include<string>
-#include<concepts>
+#include <string>
+#include <concepts>
+#include <memory>
+#include <utility>
 
 #include "HttpRequest.hpp"
 #include "nlohmann/json.hpp"
@@ -13,18 +15,25 @@ template<typename Res>
 concept ResponseChecker = std::is_class_v<Res>;
 
 template<typename Res>
-concept RequestCheck = std::is_class_v<Res> && std::derived_from<Res, AresWasmWorker::HttpRequest>;
+concept RequestCheck =
+    std::is_class_v<Res> &&
+    std::derived_from<Res, AresWasmWorker::HttpRequest>;
 
 namespace AresWasmWorker {
     template<ResponseChecker BodyType>
     struct HttpResponse {
         uint32_t status;
         std::shared_ptr<std::map<std::string, std::string>> headers;
-
         BodyType body;
 
-        explicit HttpResponse(uint32_t status, std::shared_ptr<std::map<std::string, std::string> > headers,
-                              std::string body) : status(status), headers(std::move(headers)), body(body) {
+        explicit HttpResponse(
+            uint32_t status,
+            std::shared_ptr<std::map<std::string, std::string>> headers,
+            BodyType body
+        )
+            : status(status),
+              headers(std::move(headers)),
+              body(std::move(body)) {
         }
     };
 }
