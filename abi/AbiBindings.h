d53f77e8388c92051cefa35647dcbeea17171c69
TheArchitect123 <dan.developer789@gmail.com>
2026-03-14 18:54:07 +1100

    Update AbiBindings.h

--- abi/AbiBindings.h
+++ abi/AbiBindings.h
@@ -2,17 +2,36 @@
 
 #include <cstdint>
 
+#define WASM_IMPORT(module, name) __attribute__((import_module(module), import_name(name)))
+
 extern "C" {
-    uint32_t abi_http_fetch_blocking_async(uint32_t request_json_cstr_ptr);
-    uint32_t abi_http_response_get_status(uint32_t response_id);
-    uint32_t abi_http_response_get_body_len(uint32_t response_id);
-    uint32_t abi_http_response_copy_body(uint32_t response_id, uint32_t out_ptr, uint32_t max_len);
-    uint32_t abi_http_response_copy_header(uint32_t response_id, uint32_t key_cstr_ptr, uint32_t out_ptr, uint32_t max_len);
-    void abi_http_response_free(uint32_t response_id);
+    uint32_t abi_http_fetch_blocking_async(uint32_t request_json_cstr_ptr)
+        WASM_IMPORT("env", "abi_http_fetch_blocking_async");
 
-    uint32_t abi_http_get_user_agent_name();
-    void abi_log(uint32_t message_cstr_ptr);
+    uint32_t abi_http_response_get_status(uint32_t response_id)
+        WASM_IMPORT("env", "abi_http_response_get_status");
 
-    uint32_t alloc(uint32_t size);
-    void free_mem(uint32_t ptr, uint32_t size);
+    uint32_t abi_http_response_get_body_len(uint32_t response_id)
+        WASM_IMPORT("env", "abi_http_response_get_body_len");
+
+    uint32_t abi_http_response_copy_body(uint32_t response_id, uint32_t out_ptr, uint32_t max_len)
+        WASM_IMPORT("env", "abi_http_response_copy_body");
+
+    uint32_t abi_http_response_copy_header(uint32_t response_id, uint32_t key_cstr_ptr, uint32_t out_ptr, uint32_t max_len)
+        WASM_IMPORT("env", "abi_http_response_copy_header");
+
+    void abi_http_response_free(uint32_t response_id)
+        WASM_IMPORT("env", "abi_http_response_free");
+
+    uint32_t abi_http_get_user_agent_name()
+        WASM_IMPORT("env", "abi_http_get_user_agent_name");
+
+    void abi_log(uint32_t message_cstr_ptr)
+        WASM_IMPORT("env", "abi_log");
+
+    uint32_t alloc(uint32_t size)
+        WASM_IMPORT("env", "alloc");
+
+    void free_mem(uint32_t ptr, uint32_t size)
+        WASM_IMPORT("env", "free_mem");
 }
